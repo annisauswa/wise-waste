f96eda69efdb2f31b03e8338332817993ce87193
Angelicaviera <angelica.callysta.viera.mail.ugm.ac.id>
2023-09-21 09:43:38 +0700

    Menambahkan attribute dan method pada class Catalogue dan Sell

--- wise-waste/Sell.cs
+++ wise-waste/Sell.cs
@@ -8,5 +8,30 @@ namespace wise_waste
 {
     internal class Sell
     {
+        public string Category { get; private set; }
+        public int Weight { get; private set; }
+        public int Price { get; private set; }
+        public int Amount => Weight * Price;
+        public string Address { get; private set; }
+
+    // Define a constructor for user input
+        public SaleItem(string category, int weight, int price, string address)
+        {
+            Category = category;
+            Weight = weight;
+            Price = price;
+            Address = address; // Taken from Profile database
+        }
+
+        public confirmTransaction()
+        {
+            //show the data filled from database and ask for user confirmation
+            Console.WriteLine($"Category: {Category}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Amount: {Amount}");
+            Console.WriteLine($"Address {Address}");
+        }
+
     }
 }
