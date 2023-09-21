cb338b93a76582be760650ef576e736deeb68846
diandranpn <diandra.nabila.putri.nugroho@gmail.com>
2023-09-21 11:39:46 +0700

    Menambahkan attribute pada kelas TransactionHistory

--- wise-waste/TransactionHistory.cs
+++ wise-waste/TransactionHistory.cs
@@ -8,5 +8,25 @@ namespace wise_waste
 {
     internal class TransactionHistory
     {
+        public string Category { get; private set; }
+        public int Weight { get; private set; }
+        public int Price { get; private set; }
+        public string Address { get; private set; }
+
+        public TransactionHistory(string category, int weight, int price, string address)
+        {
+            Category = category;
+            Weight = weight;
+            Price = price;
+            Address = address; // Taken from Profile database
+        }
+
+        public void ShowTransactionHistory()
+        {
+            Console.WriteLine($"Category: {Category}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Address: {Address}");
+        }
     }
 }
