2b57215d6f9827ade496b2875418d08d49bb1898
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-11-28 13:49:56 +0700

    feat: create api

---
+++ api/Services/SellRepository.cs
@@ -0,0 +1,19 @@
+﻿using api.Models;
+
+namespace api.Services
+{
+    public class SellRepository
+    {
+        private readonly SellDbContext _context;
+
+        public SellRepository(SellDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<SellDbContext> GetAllSell()
+        {
+            return _context.Sells.ToList();
+        }
+    }
+}
